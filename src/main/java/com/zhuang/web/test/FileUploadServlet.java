358e552f2fe658f485ca7e6acc30612769b8259d
zwb <zwb@DESKTOP-PQGBP8T>
2016-12-02 23:33:22 +0800

    aa

--- src/main/java/com/zhuang/web/test/FileUploadServlet.java
+++ src/main/java/com/zhuang/web/test/FileUploadServlet.java
@@ -1,11 +1,18 @@
 package com.zhuang.web.test;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
 public class FileUploadServlet extends HttpServlet {
@@ -15,8 +22,31 @@ public class FileUploadServlet extends HttpServlet {
 
 		Boolean isMultipart = ServletFileUpload.isMultipartContent(request);
 
-		response.getWriter().append(isMultipart.toString());
-		
+		if(isMultipart)
+		{
+			
+			// Create a factory for disk-based file items
+			DiskFileItemFactory factory = new DiskFileItemFactory();
+
+			// Configure a repository (to ensure a secure temp location is used)
+			ServletContext servletContext = this.getServletConfig().getServletContext();
+			File repository = (File) servletContext.getAttribute("javax.servlet.context.tempdir");
+			factory.setRepository(repository);
+
+			// Create a new file upload handler
+			ServletFileUpload upload = new ServletFileUpload(factory);
+
+			// Parse the request
+			try {
+				
+				List<FileItem> items = upload.parseRequest(request);
+				
+				
+			} catch (FileUploadException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
