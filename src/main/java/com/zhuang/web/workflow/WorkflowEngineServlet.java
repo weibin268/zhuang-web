26e7a036ff1c435a6c3be3d72489c012289cf4a9
weibin268 <binweizhuang@hotmail.com>
2017-01-17 18:02:49 +0800

    aaa

--- src/main/java/com/zhuang/web/workflow/WorkflowEngineServlet.java
+++ src/main/java/com/zhuang/web/workflow/WorkflowEngineServlet.java
@@ -24,8 +24,10 @@ public class WorkflowEngineServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+
+		String amount =request.getParameter("amount");
+		
+		response.getWriter().append("amount: ").append(amount);
 	}
 
 	/**
