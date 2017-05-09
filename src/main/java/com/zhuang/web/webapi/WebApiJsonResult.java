8cf2f050f35d45b698f5bfe7100ffe7254df49bb
weibin268 <binweizhuang@hotmail.com>
2017-05-09 11:34:45 +0800

    aa

---
+++ src/main/java/com/zhuang/web/webapi/WebApiJsonResult.java
@@ -0,0 +1,46 @@
+package com.zhuang.web.webapi;
+
+public class WebApiJsonResult {
+
+	public boolean isSuccess() {
+		return success;
+	}
+
+	public void setSuccess(boolean success) {
+		this.success = success;
+	}
+
+	public boolean isValid() {
+		return valid;
+	}
+
+	public void setValid(boolean valid) {
+		this.valid = valid;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public Object getData() {
+		return data;
+	}
+
+	public void setData(Object data) {
+		this.data = data;
+	}
+
+	private boolean success;
+	
+	private boolean valid;
+	
+	private String message;
+	
+	private Object data;
+
+	
+}
