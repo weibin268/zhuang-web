2ef61d9ed7866a50dcddf4019f6168a04f80ca1c
weibin268 <binweizhuang@hotmail.com>
2017-07-08 17:08:11 +0800

    aa

--- src/main/webapp/modules/upms/js/main_tab_ref.js
+++ src/main/webapp/modules/upms/js/main_tab_ref.js
@@ -1,10 +1,13 @@
+(function () {
+    var baseUrl="../../..";
+    document.write("<link rel='stylesheet' href='"+baseUrl+"/js/jquery-easyui/themes/bootstrap/easyui.css'>");
+    document.write("<link rel='stylesheet' href='"+baseUrl+"/js/jquery-easyui/themes/icon.css'>");
+    document.write( "<script src='"+baseUrl+"/js/jquery-easyui/jquery.min.js'></script>");
 
-document.write("<link rel='stylesheet' href='../../../js/jquery-easyui/themes/bootstrap/easyui.css'>");
-document.write("<link rel='stylesheet' href='../../../js/jquery-easyui/themes/icon.css'>");
-document.write( "<script src='../../../js/jquery-easyui/jquery.min.js'></script>");
+    document.write("<script src='../js/base.js'></script>");
 
-document.write("<script src='../js/base.js'></script>");
+    document.write("<script src='"+baseUrl+"/js/jquery-easyui/jquery.easyui.min.js'></script>");
+    document.write("<script src='"+baseUrl+"/js/jquery-easyui/locale/easyui-lang-zh_CN.js'></script>");
 
-document.write("<script src='../../../js/jquery-easyui/jquery.easyui.min.js'></script>");
-document.write("<script src='../../../js/jquery-easyui/locale/easyui-lang-zh_CN.js'></script>");
+})();
 
