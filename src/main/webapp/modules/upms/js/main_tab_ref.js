7330b30b69befae47ac2b2b9929d3f89a8404278
weibin268 <binweizhuang@hotmail.com>
2017-07-08 18:11:42 +0800

    aa

--- src/main/webapp/modules/upms/js/main_tab_ref.js
+++ src/main/webapp/modules/upms/js/main_tab_ref.js
@@ -1,14 +1,12 @@
 (function () {
+    
+    var baseUrl="../../..";
 
     document.write("<script src='../js/base.js'></script>");
-
-    var baseUrl="../../..";
 
     document.write("<link rel='stylesheet' href='"+baseUrl+"/js/jquery-easyui/themes/bootstrap/easyui.css'>");
     document.write("<link rel='stylesheet' href='"+baseUrl+"/js/jquery-easyui/themes/icon.css'>");
     document.write( "<script src='"+baseUrl+"/js/jquery-easyui/jquery.min.js'></script>");
-
-
     document.write("<script src='"+baseUrl+"/js/jquery-easyui/jquery.easyui.min.js'></script>");
     document.write("<script src='"+baseUrl+"/js/jquery-easyui/locale/easyui-lang-zh_CN.js'></script>");
 
